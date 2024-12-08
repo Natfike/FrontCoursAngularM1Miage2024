23629062fc514023ee3b571a033578239729cddb
Natifke <natifke.hain@gmail.com>
2024-12-08 12:54:04 +0100

    a

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const path = require('path');
+//Install express server
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static((__dirname + '/dist/assignment-app/')));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/assignment-app/browser"));
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/assignment-app/src/index.html'));
+app.get("/*", function (req, res) {
+ res.sendFile(path.join(__dirname + "/dist/assignment-app/browser/index.html"));
 });
-
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8081);
